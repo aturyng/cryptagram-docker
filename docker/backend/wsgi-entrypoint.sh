1e3b88da649ee6707701d03c363a3d1f31d7cd6a
turyng <turyng@proton.me>
2023-07-16 16:56:28 +0200

    Add initial docker files (working version for dev profile)

---
+++ docker/backend/wsgi-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+until cd /app/backend
+do
+    echo "Waiting for server volume..."
+done
+
+until ./manage.py migrate
+do
+    echo "Waiting for db to be ready..."
+    sleep 2
+done
+
+python3 ./manage.py collectstatic --noinput
+
+gunicorn messenger_project.wsgi --bind 0.0.0.0:8000 #--workers 4 --threads 4
+#./manage.py runserver 0.0.0.0:8003
